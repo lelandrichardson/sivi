7757da7d0f51063ebebc6a7f6f0e32aad7e62b0b
Leland Richardson <lelandr@google.com>
2021-07-09 16:10:29 -0700

    Add PID to DomeMotor and fill out implementation

--- src/domectl/DomeMotor.cpp
+++ src/domectl/DomeMotor.cpp
@@ -1,20 +1,57 @@
 #include "DomeMotor.h"
 
+#define ENCODER_STEPS_PER_ROTATION 2400
+#define WHEEL_DIAMETER_INCHES 2.975
+#define DOME_DIAMETER_INCHES 120.2
+
+DomeMotor::DomeMotor(uint8_t rightPin, uint8_t leftPin) : 
+  pin_pwm_r(rightPin), 
+  pin_pwm_l(leftPin), 
+  pid(PID(
+    &input,
+    &output,
+    &setPoint,
+    // TODO: make these tweakable
+    kp,
+    ki,
+    kd,
+    DIRECT
+  )) 
+{
+  
+}
+
 boolean DomeMotor::begin(boolean A, boolean B) {
+  // TODO: make this updateable
+  // It's unclear to me if we should just figure out what this number is and make it a constant, or have the software update this as it
+  // goes. It's probably not worth updating it since the dome will rarely undergo multiple rotations in the course of a night.
+  stepsPerDomeRotation = ENCODER_STEPS_PER_ROTATION * DOME_DIAMETER_INCHES / WHEEL_DIAMETER_INCHES; // PI cancels out
+  automatic = false;
   encoderPosition = 0; // TODO: save/restore?
   currentVelocity = 0;
   lastVelocityTime = micros();
   encoderState = stateOf(A, B);
   pinMode(pin_pwm_r, OUTPUT);
   pinMode(pin_pwm_l, OUTPUT);
+
+  // TODO: make configurable
+  minimumErrorDistance = 1000;
+  // TODO: make pid constants tweakable
+  // TODO: load constants and other values from EEPROM?
+  // TODO: make sure we use motor pins that won't move motor during reset
+  pid.SetMode(automatic);
+  pid.SetControllerDirection(DIRECT); // DIRECT or REVERSE
+  pid.SetOutputLimits(-255, 255);
+  pid.SetSampleTime(200); // 200ms
+  
   return true;
 }
 
 void DomeMotor::loop() {
   unsigned long currentTime = micros();
   unsigned long encTimeElapsed = currentTime - lastVelocityTime;
-  // update velocity once per second, keep track of 8 seconds worth of data
-  // for a weighted average
+
+  // update velocity 2x per second
   if (encTimeElapsed > 500 * MICROS_PER_MS) {
     long positionDelta = encoderPosition - lastVelocityPosition;
     currentVelocity = (float)positionDelta / encTimeElapsed;
@@ -22,20 +59,34 @@ void DomeMotor::loop() {
     lastVelocityTime = currentTime;
   }
 
-  // update voltage sent to motor
-  int voltageTarget = _motorVoltageTarget;
-  if (currentMotorVoltage < voltageTarget) {
-    currentMotorVoltage = min(voltageTarget, currentMotorVoltage + 10);
-  } else if (currentMotorVoltage > voltageTarget) {
-    currentMotorVoltage = max(voltageTarget, currentMotorVoltage - 10);
+  input = (double)equivalentSteps(encoderPosition, (long)setPoint, stepsPerDomeRotation);
+
+  if (pid.Compute()) {
+    setMotorSpeed(output);
   }
-  if (currentMotorVoltage >= 0) {
-    analogWrite(pin_pwm_l, 0);
-    analogWrite(pin_pwm_r, currentMotorVoltage);
-  } else if (currentMotorVoltage < 0) {
-    analogWrite(pin_pwm_l, -currentMotorVoltage);
-    analogWrite(pin_pwm_r, 0);
+
+  if (!automatic) {
+    // update voltage sent to motor
+    int voltageTarget = _motorVoltageTarget;
+    if (currentMotorVoltage < voltageTarget) {
+      currentMotorVoltage = min(voltageTarget, currentMotorVoltage + 10);
+    } else if (currentMotorVoltage > voltageTarget) {
+      currentMotorVoltage = max(voltageTarget, currentMotorVoltage - 10);
+    }
+    // TODO: we need to create some sort of a debounce timer here to only
+    // update the manual motor voltage once per some time interval
+    setMotorSpeed(currentMotorVoltage);
   }
+}
+
+void DomeMotor::setMotorSpeed(double speed) {
+  if (speed >= 0) {
+      analogWrite(pin_pwm_l, 0);
+      analogWrite(pin_pwm_r, (uint32_t)speed);
+    } else if (speed < 0) {
+      analogWrite(pin_pwm_l, (uint32_t)(-1 * speed));
+      analogWrite(pin_pwm_r, 0);
+    }
 }
 
 long DomeMotor::getPositionInSteps() {
@@ -60,67 +111,129 @@ float DomeMotor::getVelocityInStepsPerSec() {
 }
 
 boolean DomeMotor::isSlewing() {
+  if (_motorVoltageTarget != 0 || abs(output) > 1) {
+    return false;
+  }
   return true;
 }
 
 boolean DomeMotor::isParked() {
-  return false;
+  return !isSlewing() && isEffectivelyAtPosition(parkPositionInSteps);
 }
 
 boolean DomeMotor::isHome() {
-  return false;
+  return !isSlewing() && isEffectivelyAtPosition(homePositionInSteps);
 }
 
 float DomeMotor::getHomePositionInDegrees() {
-  return 0;
+  return stepsToDegrees(homePositionInSteps);
 }
 
 long DomeMotor::getHomePositionInSteps() {
-  return 0;
+  return homePositionInSteps;
 }
 
 void DomeMotor::syncCurrentPositionAsDegrees(float degrees) {
-
+  
+  // TODO
 }
 
 void DomeMotor::setPark(float degrees) {
-
-}
-
-void DomeMotor::setPark(long steps) {
-
+  setPark(degreesToSteps(degrees));
 }
 
 void DomeMotor::setPark() {
+  setPark(encoderPosition);
+}
 
+void DomeMotor::setPark(long steps) {
+  parkPositionInSteps = steps;
 }
 
 void DomeMotor::slewTo(float degrees) {
-
+  slewTo(degreesToSteps(degrees));
 }
 
 void DomeMotor::slewTo(long steps) {
-
+  automaticMode();
+  setPoint = nearestEquivalentSteps(steps);
 }
 
 void DomeMotor::slewToHome() {
-
+  automaticMode();
+  setPoint = nearestEquivalentSteps(homePositionInSteps);
 }
 
 void DomeMotor::slewToPark() {
-
+  automaticMode();
+  setPoint = nearestEquivalentSteps(parkPositionInSteps);
 }
 
 void DomeMotor::slewRight() {
+  manualMode();
   _motorVoltageTarget = 255;
 }
 
 void DomeMotor::slewLeft() {
+  manualMode();
   _motorVoltageTarget = -255;
 }
 
 void DomeMotor::stop() {
+  manualMode();
   _motorVoltageTarget = 0;
+}
+
+void DomeMotor::manualMode() {
+  if (automatic) {
+    automatic = false;
+    _motorVoltageTarget = 0;
+    pid.SetMode(automatic);
+  }
+}
+
+void DomeMotor::automaticMode() {
+  if (!automatic) {
+    automatic = true;
+    _motorVoltageTarget = 0;
+    pid.SetMode(automatic);
+  }
+}
+
+boolean DomeMotor::isEffectivelyAtPosition(long steps) {
+  long a = steps % stepsPerDomeRotation;
+  long b = encoderPosition % stepsPerDomeRotation;
+  return abs(a - b) < minimumErrorDistance;
+}
+
+long DomeMotor::degreesToSteps(float degrees) {
+  return (long)(degrees / 360.0 * stepsPerDomeRotation);
+}
+
+float DomeMotor::stepsToDegrees(long steps) {
+  double fractionRotated = (double)((steps % stepsPerDomeRotation)) / stepsPerDomeRotation;
+  return fractionRotated * 360.0;
+}
+
+long equivalentSteps(long steps, long relativeTo, long mod) {
+  long pos = relativeTo % mod;
+  long target = steps % mod;
+  long other;
+  if (target > pos) {
+    other = target - mod;
+  } else {
+    other = target + mod;
+  }
+
+  if (abs(pos - target) < abs(pos - other)) {
+    return target;
+  } else {
+    return other;
+  }
+}
+
+long DomeMotor::nearestEquivalentSteps(long steps) {
+  return equivalentSteps(steps, encoderPosition, stepsPerDomeRotation);
 }
 
 // STATES (AB):
--- src/domectl/DomeMotor.h
+++ src/domectl/DomeMotor.h
@@ -1,14 +1,12 @@
 #include <Arduino.h>
 #include <analogWrite.h>
+#include <PID_v1.h>
 #include "../shared/Constants.h"
 
-#define ENCODER_STEPS_PER_ROTATION 2400
-#define WHEEL_DIAMETER_INCHES 2.975
-#define DOME_DIAMETER_INCHES 120.2
 
 class DomeMotor {
   public:
-    DomeMotor(uint8_t rightPin, uint8_t leftPin) : pin_pwm_r(rightPin), pin_pwm_l(leftPin) {}
+    DomeMotor(uint8_t rightPin, uint8_t leftPin);
     boolean begin(boolean A, boolean B);
     void encoderPulse(boolean A, boolean B);
     void loop();
@@ -35,6 +33,28 @@ class DomeMotor {
 
   private:
     uint8_t stateOf(boolean A, boolean B);
+    long degreesToSteps(float degrees);
+    float stepsToDegrees(long steps);
+    long nearestEquivalentSteps(long steps);
+    void manualMode();
+    void automaticMode();
+    boolean isEffectivelyAtPosition(long steps);
+
+    void setMotorSpeed(double speed);
+
+    boolean automatic;
+
+    double input;
+    double output;
+    double setPoint;
+
+    double kp;
+    double ki;
+    double kd;
+    PID pid;
+
+    long parkPositionInSteps;
+    long homePositionInSteps;
 
     volatile long encoderPosition = 0;
     volatile uint8_t encoderState = 0;
@@ -43,9 +63,10 @@ class DomeMotor {
     // velocity in encoder steps per microsecond
     float currentVelocity = 0.0;
 
-    // It's unclear to me if we should just figure out what this number is and make it a constant, or have the software update this as it
-    // goes. It's probably not worth updating it since the dome will rarely undergo multiple rotations in the course of a night.
-    static const unsigned long stepsPerDomeRotation = ENCODER_STEPS_PER_ROTATION * DOME_DIAMETER_INCHES / WHEEL_DIAMETER_INCHES; // PI cancels out
+    unsigned long stepsPerDomeRotation;
+    unsigned long minimumErrorDistance;
+    // TODO: is this equivalent to homePositionInSteps???
+    long stepsToTrueNorthOffset = 0;
 
     volatile int _motorVoltageTarget = 0;
     int currentMotorVoltage = 0;
